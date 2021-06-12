03e9380f177fd217e1e096e320c5fd58d94dc91e
Grzegorz Matuszak <geniesis@o2.pl>
2021-06-12 11:09:25 +0200

    Init

---
+++ Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace ThingSpeak
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(34, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CPU";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(34, 192);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 39);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Memory";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 27F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(196, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 52);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "-";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 27F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(196, 239);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 52);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "-";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 390);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "ThingSpeakExample";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Net;
+
+namespace ThingSpeak
+{
+    public partial class Form1 : Form
+    {
+        private PerformanceCounter CPUCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+        private PerformanceCounter MemCounter = new PerformanceCounter("Memory", "Available MBytes");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            float cpu = CPUCounter.NextValue();
+            float mem = MemCounter.NextValue();
+            label3.Text = cpu.ToString("f2");
+            label4.Text = mem.ToString("f2");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            float cpu = CPUCounter.NextValue();
+            float mem = MemCounter.NextValue();
+            label3.Text = cpu.ToString("f2");
+            label4.Text = mem.ToString("f2");
+
+            const string WRITEKEY = "";
+            string strUpdateBase = "http://api.thingspeak.com/update";
+            string strUpdateURI = strUpdateBase + "?key=" + WRITEKEY;
+            HttpWebRequest ThingsSpeakReq;
+            HttpWebResponse ThingsSpeakResp;
+
+            strUpdateURI += "&field1=" + label3.Text;
+            strUpdateURI += "&field2=" + label4.Text;
+
+            ThingsSpeakReq = (HttpWebRequest)WebRequest.Create(strUpdateURI);
+        }
+    }
+}
